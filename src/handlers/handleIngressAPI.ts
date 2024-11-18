920ee30ea4d99c642eedb0724187275f800d8e88
Orkun <orkuncakilkaya@gmail.com>
2024-11-18 14:12:50 +0300

    chore: add helpful proxy secret checker log

--- src/handlers/handleIngressAPI.ts
+++ src/handlers/handleIngressAPI.ts
@@ -1,4 +1,4 @@
-import { IntegrationEnv, isOpenClientResponseEnabled } from '../env'
+import { IntegrationEnv, isOpenClientResponseEnabled, isProxySecretSet } from '../env'
 import {
   addProxyIntegrationHeaders,
   addTrafficMonitoringSearchParamsForVisitorIdRequest,
@@ -12,6 +12,9 @@ import { getIngressBackendByRegion } from '../utils/getIngressBackendByRegion'
 import { CacheOverride } from 'fastly:cache-override'
 
 async function makeIngressRequest(receivedRequest: Request, env: IntegrationEnv) {
+  if (!isProxySecretSet) {
+    console.log("PROXY_SECRET is not set in the integration's Secret store, your integration is not working correctly")
+  }
   const url = new URL(receivedRequest.url)
   url.pathname = ''
   addTrafficMonitoringSearchParamsForVisitorIdRequest(url)
