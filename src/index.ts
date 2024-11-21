d4f017921d72240fa8db09b3d9a999fcc3bf3e7f
Orkun <orkuncakilkaya@gmail.com>
2024-11-21 12:36:25 +0300

    refactor: move getEnvObject to env.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 /// <reference types="@fastly/js-compute" />
 import { handleReq } from './handler'
-import {
-  agentScriptDownloadPathVarName,
-  decryptionKeyVarName,
-  getResultPathVarName,
-  IntegrationEnv,
-  openClientResponseVarName,
-  proxySecretVarName,
-} from './env'
+import { getEnvObject } from './env'
 import { returnHttpResponse } from './utils/returnHttpResponse'
 import { createFallbackErrorResponse } from './utils'
 import { setClientIp } from './utils/clientIp'
-import { getConfigStore, getSecretStore } from './utils/getStore'
 
 addEventListener('fetch', (event) => event.respondWith(handleRequest(event)))
 
@@ -24,29 +16,5 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
   } catch (e) {
     console.error(e)
     return createFallbackErrorResponse('something went wrong')
-  }
-}
-
-async function getEnvObject(): Promise<IntegrationEnv> {
-  let configStore
-  try {
-    configStore = getConfigStore()
-  } catch (e) {
-    console.error(e)
-  }
-
-  let secretStore
-  try {
-    secretStore = getSecretStore()
-  } catch (e) {
-    console.error(e)
-  }
-
-  return {
-    AGENT_SCRIPT_DOWNLOAD_PATH: configStore?.get(agentScriptDownloadPathVarName) ?? null,
-    GET_RESULT_PATH: configStore?.get(getResultPathVarName) ?? null,
-    OPEN_CLIENT_RESPONSE_PLUGINS_ENABLED: configStore?.get(openClientResponseVarName) ?? null,
-    PROXY_SECRET: (await secretStore?.get(proxySecretVarName))?.plaintext() ?? null,
-    DECRYPTION_KEY: (await secretStore?.get(decryptionKeyVarName))?.plaintext() ?? null,
   }
 }
